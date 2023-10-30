94b70644f9e6f48df07134f0a827d4343fa81a71
Lara, Benjamin R <benjamin.lara@laratechs.com>
2023-10-30 14:00:22 -0400

    security(minecraft): :lock: install leaner JRE over JDK

--- game-servers/minecraft/scripts/install-minecraft-server.sh
+++ game-servers/minecraft/scripts/install-minecraft-server.sh
@@ -1,4 +1,4 @@
-sudo apt-get install unzip openjdk-17-jdk -y
+sudo apt-get install unzip openjdk-17-jre -y
 
 wget https://api.papermc.io/v2/projects/paper/versions/1.20.2/builds/259/downloads/paper-1.20.2-259.jar -O /tmp/server.jar
 
